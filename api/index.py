8b8ad91b69bbea7145593b37195de7836680e69f
tankokhua <tankokhua@outlook.com>
2025-06-14 23:41:30 +0800

    Update app

--- api/index.py
+++ api/index.py
@@ -5,15 +5,6 @@ import re
 
 app = Flask(__name__)
 
-#@app.route('/')
-#def home():
-#    return 'Hello, World!!!'
-
-@app.route('/result')
-def result():
-    dict = {'phy': 50, 'che': 60, 'maths': 70}
-    return render_template("result.html", result = dict)
-
 @app.route('/about')
 def about():
     return 'About'
