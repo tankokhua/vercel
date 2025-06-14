bb67cd4567e02bccb813e7f662e1fe929678a019
tankokhua <tankokhua@outlook.com>
2025-06-14 21:12:20 +0800

    update index.py

--- api/index.py
+++ api/index.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!!!'
+#@app.route('/')
+#def home():
+#    return 'Hello, World!!!'
+
+@app.route('/', methods=['GET', 'POST'])
+def MainHandler():
+    return render_template("index.htm", wordlength="", letters="", filters="")
 
 @app.route('/about')
 def about():
